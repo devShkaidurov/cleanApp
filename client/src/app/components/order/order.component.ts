39bd3d66d95ce47145fa157ab6b3ac984d31f717
devShkaidurov <private.shkaidurov@gmail.com>
2024-04-15 22:24:14 +0400

    Photos preview

--- client/src/app/components/order/order.component.ts
+++ client/src/app/components/order/order.component.ts
@@ -52,4 +52,70 @@ export class OrderComponent implements OnInit {
   getObjectType(type: number): string {
     return this.objectType.find(item => item.key === type)?.value;
   }
+
+  uploadPhotos(): void {
+    const input = document.getElementById("imageLoader") as HTMLInputElement;
+    const preview = document.querySelector(".preview");
+      while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+      }
+      let curFiles = input.files;
+      const list = document.createElement("ol");
+      list.style.display = "flex";
+      list.style.width = "100%";
+      list.style.alignItems = "center";
+      list.style.justifyContent = "space-evenly";
+      list.style.border = "solid 1px red;";
+      list.style.flexDirection = "row";
+      list.style.margin = "0px";
+      list.style.padding = "0px";
+      preview.appendChild(list);
+  
+      const count = curFiles.length > 3 ? 3 : curFiles.length;
+
+      for (let i = 0; i < count; i++) {
+        const file = curFiles[i];
+        const listItem = document.createElement("li");
+        listItem.style.listStyleType = "none";
+        listItem.style.marginRight = "5px";
+        
+        if (this.validFileType(file)) {
+          const image = document.createElement("img");
+          image.style.height = "64px";
+          image.src = URL.createObjectURL(file);
+          image.alt = image.title = file.name;
+          listItem.appendChild(image);
+        } 
+        list.appendChild(listItem);
+      }
+    }
+
+    fileTypes = [
+      "image/apng",
+      "image/bmp",
+      "image/gif",
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/tiff",
+      "image/webp",
+      "image/x-icon",
+    ];
+    
+    validFileType (file: any) {
+      return this.fileTypes.includes(file.type);
+    }
+    
+    returnFileSize (number: number): string {
+      if (number < 1024) {
+        return `${number} bytes`;
+      } else if (number >= 1024 && number < 1048576) {
+        return `${(number / 1024).toFixed(1)} KB`;
+      } else if (number >= 1048576) {
+        return `${(number / 1048576).toFixed(1)} MB`;
+      }
+      return ""
+    }
+  
 }
